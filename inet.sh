7e7593232cedf89cabb0e60d0f0ebb6a4cbf231d
Luke <lukeroge@gmail.com>
2016-02-17 20:20:47 +1300

    k

--- inet.sh
+++ inet.sh
@@ -2,12 +2,17 @@
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/games:/usr/local/sbin:/usr/local/bin:/root/bin
 
-# get in/out octets
-inetin=`snmpget -Ov -v 2c -c public 192.168.1.1 IF-MIB::ifInOctets.4`
-inetout=`snmpget -Ov -v 2c -c public 192.168.1.1 IF-MIB::ifOutOctets.4`
 
-# strip crap
-inetin=$(echo $oldin | cut -c 12-)
-inetout=$(echo $oldout | cut -c 12-)
+while :
+do
+    sleep 30
+	# get in/out octets
+	inetin=`snmpget -Ov -v 2c -c public 192.168.1.1 IF-MIB::ifInOctets.4`
+	inetout=`snmpget -Ov -v 2c -c public 192.168.1.1 IF-MIB::ifOutOctets.4`
 
-curl -i -u monitor:43nu889Q3ypeuRJh6qT4 -XPOST 'http://localhost:8086/write?db=monitor' --data-binary "traffic,port=inet rx=$inetin,tx=$inetout"
+	# strip crap
+	inetin=$(echo $inetin | cut -c 12-)
+	inetout=$(echo $inetout | cut -c 12-)
+
+	curl -i -u monitor:43nu889Q3ypeuRJh6qT4 -XPOST 'http://localhost:8086/write?db=monitor' --data-binary "traffic,port=inet rx=${inetin}i,tx=${inetout}i"
+done
