64252d7cfad087b52000f746ee4dd06ad22cf154
Luke <lukeroge@gmail.com>
2016-02-22 20:08:46 +1300

    cvb

--- inet.sh
+++ inet.sh
@@ -2,17 +2,35 @@
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/games:/usr/local/sbin:/usr/local/bin:/root/bin
 
+source /tmp/inet
 
-while :
-do
-    sleep 30
-	# get in/out octets
-	inetin=`snmpget -Ov -v 2c -c public 192.168.1.1 IF-MIB::ifInOctets.7`
-	inetout=`snmpget -Ov -v 2c -c public 192.168.1.1 IF-MIB::ifOutOctets.7`
+# get current in/out octets
+in=`snmpget -Ov -v 2c -c public 192.168.1.1 IF-MIB::ifInOctets.7`
+out=`snmpget -Ov -v 2c -c public 192.168.1.1 IF-MIB::ifOutOctets.7`
 
-	# strip crap
-	inetin=$(echo $inetin | cut -c 12-)
-	inetout=$(echo $inetout | cut -c 12-)
+# strip crap
+in=$(echo $inetin | cut -c 12-)
+out=$(echo $inetout | cut -c 12-)
 
+#Get the difference between the old and current
+diffin=$((in - oldin))
+diffout=$((out - oldout))
+	
+#Calculate the bytes-per-second
+bpsin=$((diffin / sleeptime))
+bpsout=$((diffout / sleeptime))
+
+if [[ $inbps -lt 0 || $outbps -lt 0 || $kidsinbps -lt 0 || $kidsoutbps -lt 0 ]] 
+then
+
+else
 	curl -i -u monitor:43nu889Q3ypeuRJh6qT4 -XPOST 'http://localhost:8086/write?db=monitor' --data-binary "traffic,port=inet rx=${inetin}i,tx=${inetout}i"
+fi
+
+
+oldin=$in
+oldout=$out
+
+for var in oldin oldout; do
+    declare -p $var | cut -d ' ' -f 3- >> /tmp/inet
 done
